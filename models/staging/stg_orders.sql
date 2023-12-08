95c46190d0cbdebfb9290e0d214b5789dc0916d8
Joe Nokia <joenokian6@gmail.com>
2023-12-08 04:00:45 +0000

    day10.5

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,13 @@
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status,
+    case
+        when status in ('placed', 'shipped', 'completed')
+            then 'Y'
+        when status in ('returned', 'return_pending')
+            then 'N'
+    end as is_valid
 
-    from {{ source('jaffle_shop', 'orders') }}
+from {{ source('jaffle_shop', 'orders') }}
